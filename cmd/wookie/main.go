e68225eb2f9daa4c59bd9561b89bfaab52e2fcb1
Manfred Touron <m@42.am>
2015-10-29 09:19:53 +0100

    Add count and cpuprofile options

--- cmd/wookie/main.go
+++ cmd/wookie/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"runtime/pprof"
 	"strings"
 
 	"github.com/moul/wookie"
 )
 
+var cpuProfile = flag.String("cpuprofile", "", "write cpu profile to file")
+var count = flag.Int("count", 1, "amount of iterations")
+
 func main() {
-	stream, _ := os.Open(os.Args[1])
+	flag.Parse()
+
+	if *cpuProfile != "" {
+		f, err := os.Create(*cpuProfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	stream, _ := os.Open(flag.Arg(0))
 	input, _ := ioutil.ReadAll(stream)
-	wookie := wookie.NewWookie(strings.Split(strings.TrimSpace(string(input)), "\n"))
-	wookie.Compute()
-	fmt.Println(wookie.Genome.String())
+	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
+
+	for i := 0; i < *count; i++ {
+		wookie := wookie.NewWookie(lines)
+		wookie.Compute()
+		fmt.Println(wookie.Genome.String())
+	}
 }
